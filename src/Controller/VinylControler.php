df54fe72e1227dad850b5915298b4b8719b9bab4
chrm0031 <114315794+chrm0031@users.noreply.github.com>
2022-10-06 14:09:58 -0400

    Sample change

--- src/Controller/VinylControler.php
+++ src/Controller/VinylControler.php
@@ -2,7 +2,13 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Routing\Annotation\Route;
+
 class VinylControler
 {
-
+    #[Route('/')]
+    public function homepage()
+    {
+        die('Some Text)');
+    }
 }
